89aa17d1965318c03057faed444d6e184b65b27d
orphialtus <62411682+hancc1006@users.noreply.github.com>
2020-11-06 23:43:59 -0800

    Add files via upload

---
+++ public/db.js
@@ -0,0 +1,25 @@
+//17-NoSQL Activity 20
+
+const indexedDB = 
+    window.indexedDB || 
+    window.mozIndexedDB || 
+    window.webkitIndexedDB || 
+    window.msIndexedDB;
+
+let db;
+const request = window.indexedDB.open("budget",1);
+
+request.onupgradeneeded=({target})=>{
+    db = target.result;
+    const objectStore=db.createObjectStore("budget");
+    objectStore.createIndex("pending",{autoIncrement:true});
+};
+
+request.onsuccess=event=>{
+    console.log(request.result);
+};
+
+request.onerror=function(event){
+    console.log("Error"+event.target.errorCode);
+};
+
