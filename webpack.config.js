b1f2dd7cabeb722caf977ae0ff2b805da2a7dbcb
orphialtus <62411682+hancc1006@users.noreply.github.com>
2020-11-06 23:44:21 -0800

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path=require("path");
+
+const config = {
+  entry: "./Develop/server.js",
+  output: {
+    path: __dirname + "/public/dist",
+    filename: "bundle.js"
+  },
+  mode: "development",
+  plugins:[
+    new WebpackPwaManifest({
+      name:"Budget App",
+      short_name: "Budget App",
+      description:"An application for budgeting",
+      background_color: "#01579b",
+      theme_color:"#ffffff",
+      "theme-color":"#ffffff",
+      start_url:"/",
+      icons:[{
+        src:path.resolve("public/icons/icon-192x192.png"),
+        sizes:[192,512],
+      }]
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  }
+};
+module.exports = config;
